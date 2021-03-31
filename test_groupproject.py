da36ebc71ea69c28a21b4b96b9bd106a65aa3026
Yiling Du <duyiling0323@gmail.com>
2021-03-31 11:29:25 -0400

    add pytest for point function

--- test_groupproject.py
+++ test_groupproject.py
@@ -2,8 +2,11 @@ from groupproject import point
 
 def test_point():
     assert point("fruit") == 5
+    assert point("APPLE") == 5
     assert point("Animal") == 6
+    assert point("puppy") == 5
     assert point("WEATHER") == 7
+    assert point("snow") == 4
     assert point("hi") == 2
     assert point("hello") == 5
     assert point("hellohellohell") == 14
