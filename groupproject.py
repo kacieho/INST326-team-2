137e1655a348f77f045cd36a37545bb4bf627896
Maham Sohail <msohail1@umd.edu>
2021-04-09 17:38:27 -0400

    Maham

--- groupproject.py
+++ groupproject.py
@@ -6,39 +6,85 @@ Topic: Scattergories
 import random
 import string
 
-class Humanplayer():
-    """User input words based on categories and letters
+class Humanplayer:
+    """ User input words based on categories and letters
+    Attributes: 
+    HumanPlayer(string)- the humanplayer for the scategories game.
+    
     """
     def __init__(self, humanplayer):
         """initialize humanplayer
+        
+        Args: 
+        HumanPlayer(string)-the humanplayer for the scategories game.
+        
+        Side Effects:humanplayer inputs their guess to the console.
         """
         # self.humanplayer=humanplayer
         
-    def humanguess():
+    def humanguess(humanplayer, word):
+        """ Take an input word from the humanplayer and instanciate the point() method.
+        
+        Args:
+        word(string)- the word that the humanplayer inputs.
+        
+        Side Effects: print a string that contains the input.
+        """
         # human_answer=input("Please enter the guessing word: ")
         # if answer correct then call point method() to know how much points get
         # dict={"Color": red, white, }
          
-class Computerplayer():
+class Computerplayer:
     """Generate random words based on list given to it 
-    The computer player could “make mistakes”
+    The computer player could “make mistakes” 
+    
+    Attributes: 
+    computerplayer(string)- the computerplayer.
     """
     def __init__(self, computerplayer):
         """initialize computerplayer
+        
+        Args: 
+        Computerplayer(string)- the computerplayer.
+        
+        Side Effect: Print a string that contains the computerplayers guess. 
         """
-    def method2():
+    def mistake():
     """if the cate is fruit, then ...
+    Calling back the text file that relates to the topic and modify 
+    the file to limit what the computer is able to guess.
+    
+    Args: 
+    
+    Side Effects: 
     """
     # "color": pink, purple, yollow, 
     # if color, delete some color, 
     # limit what computer can guess
+    
+    # Notes: In computer player class
+    # Player chooses a difficulty level based on how fast the computerplayer can generate answers
+    # Text based menu to set difficulty level and/or set to default level. 
+    # Give users options to change difficulty level. 
+    #Generate a random number and use that to determine if the computer player will make a mistake
+    #Generate a number between 0 and 1 which will determine the threshold(~50% chance, ~90%) of the percent the computer player
+    # will make a mistake. Create a linear equation. Desmos
+    
+    
 
 def generator():
-    """Generate random letter and category to start game
-        Letter a-z
-        Category from a given list of topics
+    """Generate random letter and category to start game The Letters will be from
+        a-z.The Category will be chosen from a given list of category topics.
+         
+        Args: (#check later: if no parameters no arguments)
+        cateogory(list)- A given list of four different categories 
+        to choose from to start the game. 
+        letter(character)- A chosen letter from a-z to start the game. 
+        
+        Side Effects: Prints a category and letter for user to begin the game. 
+        (Return:)
     """
-    category_list=["Fruit", "Weather", "Weekdays","Color", "Food", "Names", "Months","Holidays", "Animals"]
+    category_list=["Fruit","Color", "Holidays", "Animals"]
     letter_string=string.ascii_letters #"abcd...z + ABCD... Z"
     answer_cate=random.choice(category_list)
     answer_letter=random.choice(letter_string)
@@ -46,27 +92,31 @@ def generator():
     
         
 def timer():
-    """
-    Set a timer to start when input screen pops up 
-    Timer will not be on screen but will be counting
-    Timer stops when user inputs word and calculates if user completed in time range
-    If within time range gets points if not no points
-    (Testing)
+    #Loops, Somepoint in the loop you need to check the time. 
+    #Python for everybody chapter with formula for loops.
+    #Before the loops start you want to capture current time and you want to check if time elapsed during 
+    #during the loop. Check if time has elapsed and if so the timer ends. 
+    
+    """ This function will set a timer when the humanplayers input 
+    screen pops up and checks if the given answer is within the time range.
+    Timer will not be on screen but will be counting,
+    the timer stops when the user inputs a word. 
+    
+    Args: 
+    
+    Side Effects: 
     """
 
 def point(word): 
-    """The funcation indicate the point system for the game
+    """ Score the word.Longer words get more points. 
+    #The function indicate the point system for the game
     
     Args:
-        one required parameter "word", the specific word
+    word(string)- one required parameter "word", the specific word
         
-    Retruns:
-        return a number (integer 2-15), which means the point
-        2: the word length is 2, and the player get 2 ponits
-        3: the word length is 3, and the player get 3 points
-        ...
-        14: the word length is 14, and the player get 14 points
-        15: the word length is 15, and the player get 15 points
+    Returns:
+    point(integer)- The point the player gains for their turn.
+    Points will range from 2-15.
     """
     # human_answer=input("Please enter the guessing word: ")
     # computer_answer=input("Please enter the guessing word: ")
@@ -77,18 +127,26 @@ def point(word):
     return point
         
 def scorekeeping(point):
-    """
-    score keeping:
-    Calling point system 
-    Applying to human and computer player
+    """ 
+    This function calls the point() system and applies to human and computer player.
+    
+    Args: 
+    point(integer)- The amount of points from each player from the point()system.
+    
+    Side Effects: Stores the points. 
     """
         
 def outcome(scorekeeping):
-    """show the out come
-    Print statement of who is winning during the time duration. 
-    States how many points each player has through half point time in the game.
-    Prints overall winner at the end of game
+    """ prints the final statement of the outcome for who is winning
+    with how many points each player has at the end of the game.
+    
+    Args: 
+    scorekeeping(function)- Calling back the score keeping function. 
+    
+    Side Effects: Printing a statement to the console.
     """
+
+
      
 """Not Sure
 def main():
