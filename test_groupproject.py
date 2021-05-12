3db0b49e550b8251e0a98e5d9c193b038b0b3a6c
Victoria Ha <vha@terpmail.umd.edu>
2021-05-12 14:05:16 -0400

    victoria

--- test_groupproject.py
+++ test_groupproject.py
@@ -1,20 +1,35 @@
 from groupproject import point
 import groupproject 
+import builtins
+from unittest import mock
+
 
 #Humanplayer
 def test_humanguess():
     #Color Category 
-    assert humanguess("Color")== "red"
-    assert humanguess("Color")=="yellow"
-    assert humanguess("Color")=="green"
+    human= groupproject.Humanplayer("humanplayer")
+    with mock.patch("builtins.input", side_effect=["red"]):
+        assert human.humanguess("Color") == ["red"]
+    # ^ the above might be the correct way, since this
+    # is the way stated on the pytest page on elms
+    # for testing functions that require input answers but
+    # we are still still getting errors,
+    # we are working with a specific category so its more difficult
+    #conclusion: joel didnt know how to help us with this test,
+    # the way the bottom assert statements wont work because we
+    #need to create an object of the humanplayer class like i did in
+    # line 10 but it still doesnt work
+    
+    assert humanguess("Color")==["yellow", "blue", "orange"]
+    assert humanguess("Color")==["green", "white"]
     #Foods Category 
-    assert humanguess("Foods")=="apple"
-    assert humanguess("Foods")=="noodles"
-    assert  humanguess("Foods")=="pizza"
+    assert humanguess("Foods")==["apple"]
+    assert humanguess("Foods")==["noodles", "hamburger"]
+    assert  humanguess("Foods")==["pizza", "soup", "salad"]
     #Holidays Category
-    assert humanguess("Holidays")=="christmas"
-    assert humanguess("Holidays")=="newyears"
-    assert  humanguess("Holidays")=="thanksgiving"
+    assert humanguess("Holidays")== ["christmas"]
+    assert humanguess("Holidays")== ["newyears", "hanukkah"]
+    assert  humanguess("Holidays")== ["thanksgiving", "easter", "mothersday"]
     #Animals Category 
     assert humanguess("Animals")=="dog"
     assert humanguess("Animals")=="cat"
@@ -22,11 +37,20 @@ def test_humanguess():
          
 #Computerplayer
 def test_catefile():
-    assert catefile("Foods") == "foods.txt"
-    assert catefile("Holidays") == "holidays.txt"
-    assert catefile("Color") == "color.txt"
-    assert catefile("Animals") == "animals.txt"
-
+    computer= groupproject.Computerplayer("humanplayer")
+    assert computer.catefile("Foods") == "foods.txt"
+    assert computer.catefile("Holidays") == "holidays.txt"
+    assert computer.catefile("Color") == "color.txt"
+    assert computer.catefile("Animals") == "animals.txt"
+    
+#^ this is correct!!!
+    
+    
+    
+    
+    
+    
+# other functions outside of class
 def test_point():
     assert point("fruit") == 5
     assert point("APPLE") == 5
@@ -38,6 +62,18 @@ def test_point():
     assert point("hello") == 5
     assert point("hellohellohell") == 14
     assert point("hellohellohello") == 15
+    
+def test_scorekeeping():
+    assert scorekeeping(12,25) == "Current Human Score: 12" 
+    "Current Computer Score: 25"
+    assert scorekeeping(9,7) == "Current Human Score: 9" 
+    "Current Computer Score: 7"
+    assert scorekeeping(34,20) == "Current Human Score: 34" 
+    "Current Computer Score: 20"
+    assert scorekeeping(33,27) == "Current Human Score: 33" 
+    "Current Computer Score: 27"
+    assert scorekeeping(18,38) == "Current Human Score: 18" 
+    "Current Computer Score: 38"
 
 def test_outcome():
     assert outcome(10, 20) == "Computer Player won by 10!"
